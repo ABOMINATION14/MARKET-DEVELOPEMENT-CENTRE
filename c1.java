5eccaaea54dd3e68051c619159c7a9ccce9fd969
deenk <deenk@localhost>
2026-07-29 13:45:11 +0530

    Initial commit

---
+++ c1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SearchProduce {
+    public static void main(String[] args) {
+        ArrayList<String> products = new ArrayList<>();
+        products.add("Tomato");
+        products.add("Potato");
+        products.add("Carrot");
+        products.add("Onion");
+        products.add("Brinjal");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter product to search: ");
+        String search = sc.nextLine();
+
+        boolean found = false;
+
+        for (String product : products) {
+            if (product.equalsIgnoreCase(search)) {
+                System.out.println("Product Found: " + product);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Product Not Found");
+        }
+    }
+}
