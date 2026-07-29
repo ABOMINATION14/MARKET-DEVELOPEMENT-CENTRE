e119f0674a0fb323d4c4575f42f2370c1212f640
Thariq Anwar A K <thariqanwar.a2024@vitstudent.ac.in>
2026-07-29 15:47:08 +0530

    Add files via upload

---
+++ c2.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> cart = new ArrayList<>();
+
+        while (true) {
+            System.out.println("\n1.Add Product");
+            System.out.println("2.Remove Product");
+            System.out.println("3.View Cart");
+            System.out.println("4.Place Order");
+            System.out.println("5.Exit");
+
+            System.out.print("Enter Choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Product Name: ");
+                    cart.add(sc.nextLine());
+                    System.out.println("Added Successfully");
+                    break;
+
+                case 2:
+                    System.out.print("Enter Product Name: ");
+                    cart.remove(sc.nextLine());
+                    System.out.println("Removed Successfully");
+                    break;
+
+                case 3:
+                    System.out.println("Cart Items: " + cart);
+                    break;
+
+                case 4:
+                    if (cart.isEmpty()) {
+                        System.out.println("Cart is Empty");
+                    } else {
+                        System.out.println("Order Placed Successfully");
+                        System.out.println("Products: " + cart);
+                        cart.clear();
+                    }
+                    break;
+
+                case 5:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        }
+    }
+}
